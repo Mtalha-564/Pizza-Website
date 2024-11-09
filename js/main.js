72e3be6bcf282682280b3980fd552e1e063824c5
M. Talha ejaz <tallahmuhammad6@gmail.com>
2024-11-09 11:39:55 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// Scrollbar navbar 
+let nav = document.querySelector(".navigation-wrap");
+window.onscroll = function (){
+if(document.documentElement.scrollTop > 20){
+nav.classList.add("scroll-on");
+}else{
+nav.classList.remove("scroll-on");
+}
+}
+// nav hide  
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a){
+a.addEventListener("click", function(){
+navCollapse.classList.remove("show");
+})
+})
+// Loader
+fadeout();
+function loader(){
+document.querySelector('.loader-container').classList.add('active');
+}
+function fadeout(){
+setTimeout(loader, 3000);
+}
